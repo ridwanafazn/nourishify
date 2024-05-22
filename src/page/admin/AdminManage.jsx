66c25350be6ea02bb59606fd986771381d046f75
Ridwan Ahmad Fauzan <ridwanafzn@gmail.com>
2024-05-22 19:24:26 +0700

    manage

--- src/page/admin/AdminManage.jsx
+++ src/page/admin/AdminManage.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Nav from "../../component/Nav3";
 
 function AdminManage() {
+  const navigate = useNavigate();
   return (
     <div>
       <Nav/>
