0bdd777a4aeb95b0885e3232f160e856f44a91a6
Ridwan Ahmad Fauzan <ridwanafzn@gmail.com>
2024-05-27 08:14:51 +0700

    css import try to fix

--- src/page/UserProfile.jsx
+++ src/page/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavbarStudent from '../component/NavbarStudent';
-import '../style/UserProfile.css';
+import '../style/ProfileStudent.css';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const UserProfile = () => {
